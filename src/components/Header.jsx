fd1c333f0cac5caf8180345a8a2377987d9fe300
Denisson Pereira Santos <denisson159@gmail.com>
2023-11-16 16:36:57 -0300

    up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,10 @@ import '../styles/components/Header.sass'
 
 const Header = () => {
   return (
-    <header>
+    <header className='principal'>
+      <div className='imagem_logo'>
+        <img src="../src/image/logo.png" alt="logo" />
+      </div>
         <nav className='menu'>
             <ul>
                 <li>Home</li>
