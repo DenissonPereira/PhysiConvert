35f6070936a9d75a235a1588e4b750df00e76f83
Denisson Pereira Santos <denisson159@gmail.com>
2023-11-19 13:13:01 -0300

    up

--- src/components/Corpo.jsx
+++ src/components/Corpo.jsx
@@ -4,6 +4,7 @@ const Corpo = () => {
   return (
     <div className='corpo'>
         <div className='caixa'>
+            <p>PhysiConvert</p>
             <div className='buscador'>
                 <input type="search" className='barra_pesquisa' placeholder="Search" /><button className='btn_ir'>Ir</button>
             </div>
