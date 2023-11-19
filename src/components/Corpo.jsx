5e33c2ac2338591d2cb87e024ee19d01124fdf98
Denisson Pereira Santos <denisson159@gmail.com>
2023-11-19 14:13:32 -0300

    up

--- src/components/Corpo.jsx
+++ src/components/Corpo.jsx
@@ -3,11 +3,20 @@ import '../styles/components/Corpo.sass'
 const Corpo = () => {
   return (
     <div className='corpo'>
-        <div className='caixa'>
-            <p>PhysiConvert</p>
-            <div className='buscador'>
-                <input type="search" className='barra_pesquisa' placeholder="Search" /><button className='btn_ir'>Ir</button>
-            </div>
+        <div className='corpo1'>
+          <div className='caixa'>
+              <p>PhysiConvert</p>
+              <div className='buscador'>
+                  <input type="search" className='barra_pesquisa' placeholder="Search" /><button className='btn_ir'>Ir</button>
+              </div>
+        </div>
+        </div>
+        <div className='conteudo'>
+          <div className='slogam'>
+            <p>
+              <strong>PhysiConvert:</strong> Simplificando o Mundo Físico, Unidade por Unidade.
+            </p>
+          </div>
         </div>
     </div>
   )
